3f7af9daac19a103ec2a484ca01fd3382521c5de
Jef <jefvanrompay@gmail.com>
2023-09-26 22:19:10 +0200

    les 26_9_23

--- eerste_django_project/mysite/polls/views.py
+++ eerste_django_project/mysite/polls/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 
 import datetime 
 from django.utils import timezone
 
 from django.template import loader
+
+from django.urls import reverse
+
 
 # Create your views here.
 
@@ -30,8 +33,17 @@ def results(request, question_id):
     response = f"You're looking at the results of question {question_id}"
     return HttpResponse(response)
 
-def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+def vote(request, question_id): 
+    question = get_object_or_404(Question, pk = question_id)
+    # print(question.choice_set.all())
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist) as e:
+        return HttpResponse(f"No Choice found \n error: \n {e}")
+    # Choice.objects.get(pk = request.POST["choice"])
+    print(selected_choice)
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id, )))
 
 
 def test_db(request):
@@ -59,26 +71,26 @@ def test_db(request):
     # q_1 = Question.objects.filter(id=1)
     # print(f"We hebben question met id {q_1[0].id} opgehaald")
 
-    # qs = Question.objects.filter(question_text__startswith = "Question")
-    # print(qs)
+    qs = Question.objects.filter(question_text__startswith = "Question")
+    print(qs)
 
-    # current_year = timezone.now().year
-    # current_day = timezone.now().day
-    # qs_2 = Question.objects.filter(pub_date__year=current_year)
-    # print(qs_2)
-    # qs_3 = Question.objects.filter(pub_date__day=current_day)
-    # print(qs_3)
+    current_year = timezone.now().year
+    current_day = timezone.now().day
+    qs_2 = Question.objects.filter(pub_date__year=current_year)
+    print(qs_2)
+    qs_3 = Question.objects.filter(pub_date__day=current_day)
+    print(qs_3)
 
-    # previous_year = (timezone.now()-datetime.timedelta(weeks=52)).year
-    # qs_4 = Question.objects.filter(pub_date__year=previous_year)
-    # print(qs_4)
+    previous_year = (timezone.now()-datetime.timedelta(weeks=52)).year
+    qs_4 = Question.objects.filter(pub_date__year=previous_year)
+    print(qs_4)
 
     q_5 = Question.objects.get(pk=1)
-    # choices = q_5.choice_set.all()
+    choices = q_5.choice_set.all()
     # print(choices)
 
-    # q_5.choice_set.create(choice_text="Not much", votes =0)
-    # q_5.choice_set.create(choice_text="The Sky")
+    q_5.choice_set.create(choice_text="Not much", votes =0)
+    q_5.choice_set.create(choice_text="The Sky")
     
     c = Choice(question = q_5, choice_text="Just Hacking", votes=0)
     # c.save()
